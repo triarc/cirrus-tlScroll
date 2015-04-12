3cf498244cd358aec66e3fab4d24bc3f71521c34
Marco Bachmann <mb@triarc-labs.com>
2015-04-12 21:06:57 +0200

    Bumped version to 0.0.1

---
+++ tlScroll.d.ts
@@ -0,0 +1,13 @@
+declare module Triarc.Scroll {
+    var scroll: ng.IModule;
+}
+declare module Triarc.Scroll {
+}
+declare module Triarc.Scroll {
+}
+declare module Triarc.Scroll {
+}
+declare module Triarc.Scroll {
+}
+declare module Triarc.Scroll {
+}
